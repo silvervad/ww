fecec55b671749cdf48ebdb2fe4b072de21b3735
Vadim Fedorov <postav@gmail.com>
2015-02-23 09:27:47 +0000

    right menu for spots

--- app/assets/javascripts/pusher_menu.js
+++ app/assets/javascripts/pusher_menu.js
@@ -1,22 +1,9 @@
-
-function changeLogo(){
-	var container = document.getElementById("st-container");
-	if ( classie.has ( container, "st-menu-open")) {
-		classie.remove (container, "st-menu-open");
-	}
-	else{
-		classie.add (container, "st-menu-open");
-	}
-}
-
 function toggleMenu(){
-	var container = document.getElementById("rama-wrap");
-	if ( classie.has ( container, "rama-wrap")) {
-		classie.remove (container, "rama-wrap");
-		classie.add (container, "rama-wrap-menu")
+	var container = document.getElementById("spot-menu");
+	if ( classie.has ( container, "menu-closed")) {
+		classie.remove (container, "menu-closed");
 	}
 	else{
-		classie.remove (container, "rama-wrap-menu");
-		classie.add (container, "rama-wrap");
+		classie.add (container, "menu-closed");
 	}
 }
